da4bbab48f3dfbb24fe644dac3fecbeac48b845c
Jessica Bilheiro <129060450+jessicabilheiro@users.noreply.github.com>
2023-04-04 15:12:04 +0200

    Update dogAge.js

--- dogAge.js
+++ dogAge.js
@@ -1,13 +1,9 @@
-// variable with my age
-const myAge = 18
-//variavel mutavel 
+const myAge = 18 
 let earlyYears = 2
 earlyYears *=10.5
-//variable with the "rest" of age
 let laterYears = myAge - 2
 //multiplying each year for dog year and reassignmening laterYears
 laterYears *=4
-//adding the 2 variables to one with the sum 
 let myAgeInDogYears = earlyYears + laterYears
 //variable with a built in method that returns strings with all lowercase letters 
 const myName = "Jessica"
